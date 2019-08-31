15b2d665e880d67b2a57087affb2a67732e229e1
Julian <ja.melendez10@uniandes.edu.co>
2019-08-31 12:46:17 -0400

    new script

---
+++ FunctionList.py
@@ -0,0 +1,24 @@
+def saddle():
+	z = 'x**2-y**2'
+	return(z)
+
+def trigonometric():
+	z = 'cos(x)*cos(y)'
+	return(z)
+
+def wave():
+	z = 'sin(sqrt(x**2+y**2))/sqrt(x**2+y**2)'
+	return(z)
+
+def exponential():
+	z = '(1-x/ 2+x**7+y**3)*exp(-x**2-y**2)'
+	return(z)
+
+def pyramid():
+	z = '1-abs(x+y)-abs(y-x)'
+	return(z)
+
+def paper():
+	z = 'sin(x*y)'
+	return(z)
+
